4a33b0ccb0d7ddd20124c14337b1055aa364d1fd
Guy Louzon <guylou@outlook.com>
2026-04-21 16:25:27 +0300

    removed stand alone styles, removed 3rd party url calls (ad tracking)

--- app/templates/admin-ui.php
+++ app/templates/admin-ui.php
@@ -6,7 +6,6 @@ if (!defined('ABSPATH')) {
 ?>
 <div class="wrap">
     <div id="main_ui">
-    <img src="https://interq.link/ads/pixel.php">
 <?php
 // Use nullsafe operator and null coalescing for PHP 8 compatibility
 $new_api_url_2 = $rss_post_importer->is_valid_key($this->options['settings']['feeds_api_key'] ?? '');
@@ -51,24 +50,3 @@ $new_version = RSS_PI_VERSION;
 
     <div class="ajax_content"></div>
 </div>
-<script type="text/javascript">
-    adroll_adv_id = "QQBLQGEK7FB4RBKP6CVHTS";
-    adroll_pix_id = "ZAMJKSSZRREUVAYSLZ2K6S";
-    (function () {
-        var _onload = function(){
-            if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
-            if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
-            var scr = document.createElement("script");
-            var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-            scr.setAttribute('async', 'true');
-            scr.type = "text/javascript";
-            scr.src = host + "/j/roundtrip.js";
-            ((document.getElementsByTagName('head') || [null])[0] ||
-                document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-        };
-        if (window.addEventListener) {window.addEventListener('load', _onload, false);}
-        else {window.attachEvent('onload', _onload)}
-    }());
-    var new_js_url = "<?php echo esc_js($new_api_url_2); ?>";
-    var new_js_version = "<?php echo esc_js($new_version); ?>";
-</script>
